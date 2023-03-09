ec98fa6de592d991a45318477844169119eb02a6
heeyeon.kang <iamhe2ng@gmail.com>
2023-03-09 23:36:48 +0900

    멀티 모듈 구성(api, admin, core)
    누락 파일 추가

---
+++ kttr-admin/src/main/java/com/crs/kttr/AdminKttrApplication.java
@@ -0,0 +1,17 @@
+package com.crs.kttr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * KTTR admin server.
+ *
+ * @author Heeyeon.
+ * @version 0.0.1
+ */
+@SpringBootApplication
+public class AdminKttrApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AdminKttrApplication.class, args);
+  }
+}
--- kttr-api/src/main/java/com/crs/kttr/KttrApplication.java
+++ kttr-api/src/main/java/com/crs/kttr/KttrApplication.java
@@ -3,11 +3,16 @@ package com.crs.kttr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * KTTR api server.
+ *
+ * @author Heeyeon.
+ * @version 0.0.1
+ */
 @SpringBootApplication
 public class KttrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KttrApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(KttrApplication.class, args);
+  }
 }
