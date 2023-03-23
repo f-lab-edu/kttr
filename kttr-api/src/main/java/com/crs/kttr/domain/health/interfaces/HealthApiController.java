fdf8ef1a4b469b8b7ac682bd50a4ae933c4fa473
heeyeon.kang <iamhe2ng@gmail.com>
2023-03-23 12:58:14 +0900

    PR #2 리뷰 반영

--- kttr-api/src/main/java/com/crs/kttr/domain/health/interfaces/HealthApiController.java
+++ kttr-api/src/main/java/com/crs/kttr/domain/health/interfaces/HealthApiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
-public class HealthController {
+public class HealthApiController {
   @GetMapping
   public ResponseEntity<Response<String>> health() {
     return Response.ok("OK");
