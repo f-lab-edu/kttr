f67e0e6b5675764c1134bc4124af3468c12223b3
heeyeon.kang <iamhe2ng@gmail.com>
2023-04-16 00:04:07 +0900

    PR#14 리뷰 반영

--- kttr-api/src/main/java/com/crs/kttr/global/ErrorRestControllerAdvice.java
+++ kttr-api/src/main/java/com/crs/kttr/global/ErrorRestControllerAdvice.java
@@ -3,10 +3,12 @@ package com.crs.kttr.global;
 import com.crs.kttr.member.exception.MemberRegisterException;
 import com.crs.kttr.global.dto.Response;
 import com.crs.kttr.global.dto.ResponseMeta;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorRestControllerAdvice {
 
@@ -15,6 +17,8 @@ public class ErrorRestControllerAdvice {
     final int errorCode = e.getCode();
     final String message = e.getMsg();
 
-    return Response.ok(e.getStatus(), new ResponseMeta(errorCode, message), null);
+    log.error("code : {}, message : {}", errorCode, message);
+
+    return Response.error(e.getStatus(), new ResponseMeta(errorCode, message), null);
   }
 }
